2315b557786062a807b31830943adba850837c31
kotoritashiro <kotori.t.530@gmail.com>
2024-10-31 14:43:05 +0900

    day1の追加

---
+++ bocco-emo/day1.py
@@ -0,0 +1,290 @@
+from bocco_tools import BoccoTools
+import json
+
+tools = BoccoTools()
+
+tools.send_speech("Hello. What's your name?")
+k=input()
+if k=="1":
+    tools.send_speech("Hello. What's your name?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("My name is BOCCO and I would be happy if you call me BOCCO-chan. Can I ask you a few questions so we can get to know each other?")
+k=input()
+if k=="1":
+    tools.send_speech("My name is BOCCO and I would be happy if you call me BOCCO-chan. Can I ask you a few questions so we can get to know each other?")
+else: 
+    tools.send_speech("Please wait a moment.")
+
+tools.send_speech("It looks so delicious.")
+k=input()
+if k=="1":
+    tools.send_speech("It looks so delicious.")
+else: 
+    tools.send_speech("Please wait a miniutes.")
+
+tools.send_speech("By the way, there is some rice in the refrigerator nearby. I'd like you to fetch some for me.")
+k=input()
+if k=="1":
+    tools.send_speech("By the way, there is some rice in the refrigerator nearby. I'd like you to fetch some for me.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("There are some foods in there. You can take it home and eat it")
+k=input()
+if k=="1":
+    tools.send_speech("There are some foods in there. You can take it home and eat it")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Thank you.")
+k=input()
+if k=="1":
+    tools.send_speech("TThank you.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("I have one task to do with you today.")
+k=input()
+if k=="1":
+    tools.send_speech("I have one task to do with you today.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Will you do it with me?”")
+k=input()
+if k=="1":
+    tools.send_speech("Will you do it with me?”")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("First, I want you to get the block on the shelf behind you.")
+k=input()
+if k=="1":
+    tools.send_speech("First, I want you to get the block on the shelf behind you.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Today I want you to make an animal with that block.")
+k=input()
+if k=="1":
+    tools.send_speech("Today I want you to make an animal with that block.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("This block is called kapla.")
+k=input()
+if k=="1":
+    tools.send_speech("This block is called kapla.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("“I want you to make one animal in 15 minutes.”")
+k=input()
+if k=="1":
+    tools.send_speech("“I want you to make one animal in 15 minutes.”")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Is there anything you don't understand about it?")
+k=input()
+if k=="1":
+    tools.send_speech("Is there anything you don't understand about it?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Are you ready?")
+k=input()
+if k=="1":
+    tools.send_speech("Are you ready?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("All right, let's begin.")
+k=input()
+if k=="1":
+    tools.send_speech("All right, let's begin.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("What kind of animal did you decide to make?")
+k=input()
+if k=="1":
+    tools.send_speech("What kind of animal did you decide to make?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Yes, that's wonderful. I can't wait to see it.")
+k=input()
+if k=="1":
+    tools.send_speech("Yes, that's wonderful. I can't wait to see it.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Do you usually have a chance to speak English?")
+k=input()
+if k=="1":
+    tools.send_speech("Do you usually have a chance to speak English?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Yes, I do. You speak English very well.")
+k=input()
+if k=="1":
+    tools.send_speech("Yes, I do. You speak English very well.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Do you like animals?")
+k=input()
+if k=="1":
+    tools.send_speech("Do you like animals?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("What kind of animal do you like best?")
+k=input()
+if k=="1":
+    tools.send_speech("What kind of animal do you like best?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("That's good. I like them too.")
+k=input()
+if k=="1":
+    tools.send_speech("That's good. I like them too.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("I'm sorry to interrupt you. I'll ask you a few questions.")
+k=input()
+if k=="1":
+    tools.send_speech("I'm sorry to interrupt you. I'll ask you a few questions.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("What year are you in college?")
+k=input()
+if k=="1":
+    tools.send_speech("What year are you in college?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("That's good.  What's your major?")
+k=input()
+if k=="1":
+    tools.send_speech("That's good.  What's your major?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("I see. Is it hard to study?")
+k=input()
+if k=="1":
+    tools.send_speech("I see. Is it hard to study?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Yes, it is. Where are you from?")
+k=input()
+if k=="1":
+    tools.send_speech("Yes, it is. Where are you from?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("It's a nice place. Is there anything famous there?")
+k=input()
+if k=="1":
+    tools.send_speech("It's a nice place. Is there anything famous there?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("That's great. The guy I was telling you about was wondering about that, too.")
+k=input()
+if k=="1":
+    tools.send_speech("That's great. The guy I was telling you about was wondering about that, too.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("It's almost finished.")
+k=input()
+if k=="1":
+    tools.send_speech("It's almost finished.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Let me know when it's done.")
+k=input()
+if k=="1":
+    tools.send_speech("Let me know when it's done.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("“Is it done? Very good.")
+k=input()
+if k=="1":
+    tools.send_speech("“Is it done? Very good.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Now, will you bring the camera near the door?")
+k=input()
+if k=="1":
+    tools.send_speech("Now, will you bring the camera near the door?")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Take a picture of your work with that camera.")
+k=input()
+if k=="1":
+    tools.send_speech("Take a picture of your work with that camera.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Is it done?")
+k=input()
+if k=="1":
+    tools.send_speech("Is it done?")
+else: 
+    tools.send_speech("Please wait a moment.")
+
+tools.send_speech("Okay, thank you.")
+k=input()
+if k=="1":
+    tools.send_speech("Okay, thank you.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("That concludes today's task.")
+k=input()
+if k=="1":
+    tools.send_speech("That concludes today's task.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Tell the person in charge of the experiment that you're done.")
+k=input()
+if k=="1":
+    tools.send_speech("Tell the person in charge of the experiment that you're done.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Thank you  for today.")
+k=input()
+if k=="1":
+    tools.send_speech("Thank you for today.")
+else: 
+    tools.send_speech("Please wait a moment")
+
+tools.send_speech("Bye-bye")
+k=input()
+if k=="1":
+    tools.send_speech("Bye-bye")
+else: 
+    tools.send_speech("Please wait a moment")
+
+
+
+
+
+
